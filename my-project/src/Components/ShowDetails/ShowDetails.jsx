7c78ae71e060513dc64131d12b04aa11de04f90e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-24 12:39:16 +0600

    show details done without link tag

--- my-project/src/Components/ShowDetails/ShowDetails.jsx
+++ my-project/src/Components/ShowDetails/ShowDetails.jsx
@@ -1,14 +1,21 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
 
 
 const ShowDetails = () => {
+
+    const navigate= useNavigate();
+
+    const handleGoBack=()=>{
+        navigate(-1);
+    }
 
     const user= useLoaderData();
 
     return (
         <div>
             <h1 className="text-2xl">{user.name}</h1>
-            <p>{user.website}</p>
+            <p className="mb-5">{user.website}</p>
+            <Link ><button onClick={handleGoBack} className="px-2 py-2 border-2 border-r-gray-100">Go Back</button></Link>
         </div>
     );
 };
--- my-project/src/Components/User/User.jsx
+++ my-project/src/Components/User/User.jsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, Links, useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 
-const User = ({user}) => {
-    const {id,name,email,phone}= user;
+const User = ({ user }) => {
+
+    const { id, name, email, phone } = user;
+
+    const navigate = useNavigate();
+
+    const handleShowDetails = () => {
+        navigate(`/user/${id}`);
+    }
+
+
+
+
     return (
         <div className="border-2 border-black text-clip p-2">
             <h2 className="text-2xl font-bold">Name- {name}</h2>
             <p>Email- {email}</p>
             <p>Phone- {phone}</p>
-            <Link className="underline"  to={`/user/${id}`}>Show Details</Link>
+            <Link className="underline" to={`/user/${id}`}>Show Details</Link><br />
+            <button onClick={handleShowDetails} className="px-2 py-2 border-2 border-r-amber-100">Show Details</button>
         </div>
     );
 };
+
+User.propTypes = {
+    user: PropTypes.object.isRequired,
+}
 
 export default User;
